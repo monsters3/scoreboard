408e026e1c271dbdea74e0427506c7a2052ee1af
darkbright <foxtori8946@gmail.com>
2019-07-20 17:41:26 +0900

    score를 증가 혹은 감소를 하기 위해서 부모가 콜백펑션을 props 로 자식에게 전달하고 자식은 콜백펑션을 호출하는 프로토타입 작성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import './App.css';
-import {Header} from './components/Header';
+import {Header} from "./components/Header";
 import {Player} from "./components/Player";
 
 class App extends React.Component {
-	//Lifting Up : 카운터 컴포넌트가 갖고 있는 로컬 state를 최상단 부몰로 올리기
-	//로직을 구현하기 위해서 lifting up 이 필요
+	// Listing UP: 카운터 컴포넌트가 갖고 있는 로컬 state를 최상단 부모로 올리기
+	// 로직을 구현하기 위해서 Lifting up이 필요
 	state = {
-		players : [
-			{name: 'LDK', score: 30, id:1},
-			{name: 'HONG', score: 40, id:2},
-			{name: 'KIM', score: 50, id:3},
-			{name: 'PARK', score: 60, id:4},
+		players: [
+			{name: 'LDK', score: 30, id: 1},
+			{name: 'HONG', score: 40, id: 2},
+			{name: 'KIM', score: 50, id: 3},
+			{name: 'PARK', score: 60, id: 4},
 		]
 	}
 
 	render() {
 		return (
 			<div className="scoreboard">
-				<Header title="My scoreboard" totalPlayers={11} />
+				<Header title="My Scoreboard" totalPlayers={11} />
 
 				{
-					this.state.players.map((player) => {
-						return(
-							<Player name={player.name} score={player.score} id={player.id} key={player.id} removePlayer={this.handleRemovePlayer} />
-						)
-					})
+					this.state.players.map((player) =>
+						<Player name={player.name} score={player.score}
+										id={player.id} key={player.id}
+										removePlayer={this.handleRemovePlayer}
+										changeScore={this.handleChangeScore} />)
 				}
 			</div>
 		)
 	}
+
 	handleRemovePlayer = (id) => {
-		console.log(id)
-		//	자식을 삭제하는 로직
+		console.log(id);
+		// 자식을 삭제하는 로직
 		this.setState(prevState => ({
 				players: prevState.players.filter(player => player.id !== id)
 			})
 		)
 	}
 
+	handleChangeScore = (id, delta) => {
+		console.log('handleChangeScore: ', id, delta);
+	}
 }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,28 @@
 import React from 'react';
 
 export class Counter extends React.Component {
+	// 1. 시간에 따라 변하는 데이터는 state라는 모델로 정의
+	// 2. state를 변경하는 방법은 setState()밖에 없다.
+	// 3. setState()는 merge(overwriting)
+	// 4. setState() 비동기로 처리된다.
+	// 이벤트 우측에는 함수 선언문이 와야 된다.
 
-	//1. 시간에 따라 변하는 데이터는 state라는 모델로 정의
-	//2. state를 변경하는 방법은 setState() 밖에 없다.
-	//3 setState()는 merge(overwriting) 된다.
-	//4 setState() 비동기 처리된다.
-	//이벤트 우측에는 함수 선언문이 와야 된다.
-
-	//arrow function 안에 쓰이는 this는 lexical this
-	changeScore = (delta) => {
-		console.log('incrementScore: ', this);
-		// this.setState({score: this.state.score + delta})
-		this.setState(prevState => {
-			return ({score: this.state.score + delta});
-		})
-	}
-
+	// arrow function안에 쓰이는 this는 lexical this
+	/*	changeScore = (delta) => {
+			console.log('incrementScore: ', this);
+			// this.state.score += 1;
+			// this.setState({score: this.state.score + delta});
+			this.setState(prevState => ({score: prevState.score + delta}));
+		}*/
 
 	render() {
 		return (
 			<div className="counter">
-				<button className="counter-action decrement" onClick={ () => {this.changeScore(-1)} }> - </button>
+				<button className="counter-action decrement"
+								onClick={() => this.props.changeScore(this.props.id, -1)}> -</button>
 				<span className="counter-score">{this.props.score}</span>
-				<button className="counter-action increment" onClick={ () => {this.changeScore(1)} }> + </button>
+				<button className="counter-action increment"
+								onClick={() => this.props.changeScore(this.props.id, 1)}> +</button>
 			</div>
 		)
 	}
